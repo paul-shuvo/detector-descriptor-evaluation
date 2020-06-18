93c70461f2046305d6ca476e6c2e7f874ac479c6
Shuvo Kumar Paul <shuvo.k.paul@gmail.com>
2020-06-18 01:27:32 -0700

    feat: added method to extract and convert cv keypoint type

--- src/imgop.py
+++ src/imgop.py
@@ -1,14 +1,18 @@
 # from skimage import data
 import matplotlib.pyplot as plt
 import numpy as np
-
-from skimage import data, transform
-from skimage.color import rgb2gray
-tform = transform.EuclideanTransform(
-    rotation=np.pi / 12.,
-    translation = (100, -20)
-    )
-original = data.astronaut()
+import seaborn as sns
+tips = sns.load_dataset("tips")
+sns.relplot(x="total_bill", y="tip", data=tips)
+plt.show()
+s = 1
+# from skimage import data, transform
+# from skimage.color import rgb2gray
+# tform = transform.EuclideanTransform(
+#     rotation=np.pi / 12.,
+#     translation = (100, -20)
+#     )
+# original = data.astronaut()
 # grayscale = rgb2gray(original)
 #
 # fig, axes = plt.subplots(1, 2, figsize=(8, 4))
@@ -20,8 +24,8 @@ original = data.astronaut()
 # ax[1].set_title("Grayscale")
 
 # fig.tight_layout()
-transformed = transform.EuclideanTransform(translation=(5,6))
-plt.imshow(original)
-plt.imshow(transformed)
-# print(data)
-plt.show()
+# transformed = transform.EuclideanTransform(translation=(5,6))
+# plt.imshow(original)
+# plt.imshow(transformed)
+# # print(data)
+# plt.show()
